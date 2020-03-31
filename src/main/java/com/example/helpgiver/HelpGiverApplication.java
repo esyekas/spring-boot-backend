97e7df59ca6ff777ddeb75d75872fb97e00b13f1
Michal Kopec <lekkoscbytu@gmail.com>
2020-03-31 14:33:21 +0200

    Create initial spring boot app
    Create with https://start.spring.io

---
+++ src/main/java/com/example/helpgiver/HelpGiverApplication.java
@@ -0,0 +1,13 @@
+package com.example.helpgiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpGiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpGiverApplication.class, args);
+	}
+
+}
