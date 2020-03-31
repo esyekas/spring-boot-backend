02dfd117183d69aad6b00f8b3c3563a9c08dde8d
andrei <andrei@shumanski.com>
2020-03-31 19:33:28 +0200

    Add Mongo support for user

---
+++ src/main/java/com/example/helpgiver/objects/User.java
@@ -0,0 +1,119 @@
+package com.example.helpgiver.objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
+public class User {
+    @org.springframework.data.annotation.Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String publicName;
+
+    private String phoneNumber;
+    private String email;
+    private String addressText;
+
+    private GeoJsonPoint addressCoordinates;
+
+    private String riskGroup;
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPublicName() {
+        return publicName;
+    }
+
+    public void setPublicName(String publicName) {
+        this.publicName = publicName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddressText() {
+        return addressText;
+    }
+
+    public void setAddressText(String addressText) {
+        this.addressText = addressText;
+    }
+
+    public GeoJsonPoint getAddressCoordinates() {
+        return addressCoordinates;
+    }
+
+    public void setAddressCoordinates(GeoJsonPoint addressCoordinates) {
+        this.addressCoordinates = addressCoordinates;
+    }
+
+    public String getRiskGroup() {
+        return riskGroup;
+    }
+
+    public void setRiskGroup(String riskGroup) {
+        this.riskGroup = riskGroup;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", publicName='" + publicName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", addressText='" + addressText + '\'' +
+                ", addressCoordinates=" + addressCoordinates +
+                ", riskGroup='" + riskGroup + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
