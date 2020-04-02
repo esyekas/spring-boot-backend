97c9890c1631f69b1a00e2ec39d74626e88dd659
Syed Kashan Ali <syed.kashan.ali@ericsson.com>
2020-04-02 11:08:41 +0200

    Fix Java version[C

--- scripts/build_service.sh
+++ scripts/build_service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+JAVA_HOME=/usr/java/jdk-11.0.6
 mvn clean package -DskipTests -f /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/pom.xml
--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+JAVA_HOME=/usr/java/jdk-11.0.6
 java -jar  /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/target/help-giver-0.0.1-SNAPSHOT.jar
