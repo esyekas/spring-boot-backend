64a3806a7a19cfcca88d516877c733151bece25e
Syed Kashan Ali <syed.kashan.ali@ericsson.com>
2020-04-02 12:24:51 +0200

    Fix stop app scriptwq

--- scripts/stop_service.sh
+++ scripts/stop_service.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 isExistApp=`pgrep java`
-kill -9 $isExistApp  
+if [[ -z $isExistApp ]]
+then
+     echo "Application is not running"
+     exit 1
+else
+    echo "Application pid $isExistApp stopping"
+    kill -9 $isExistApp
+fi
