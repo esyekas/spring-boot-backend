48fef236c5cba868dfbe2e20b33bd4bad5ce444c
Syed Kashan Ali <syed.kashan.ali@ericsson.com>
2020-04-02 13:23:13 +0200

    Another try to run the job in background

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 JAVA_HOME=/usr/java/jdk-11.0.6
-nohup java -jar  /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/target/help-giver-0.0.1-SNAPSHOT.jar &
+java -jar  /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/target/help-giver-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 echo $! > /var/run/help-giver.pid
-return 1
